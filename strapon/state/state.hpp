577ba745899313ad77fad24747c366c4c828b8b7
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-06-13 17:18:51 +0200

    Add dt to State::update

--- strapon/state/state.hpp
+++ strapon/state/state.hpp
@@ -1,12 +1,11 @@
 #ifndef STATE_HPP
 #define STATE_HPP
 
-class State
-{
+class State {
 public:
     virtual int init() = 0;
-    virtual void update() = 0;
-    virtual ~State() {}
+    virtual void update(float dt) = 0;
+    virtual ~State() = 0;
 };
 
 #endif /* end of include guard: STATE_HPP */
