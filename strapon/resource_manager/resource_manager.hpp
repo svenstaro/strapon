e4922a4fdc7a3da3859fb04b2edfa317bb387385
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-06-14 23:49:47 +0200

    ResourceManager: Fix for emscripten

--- strapon/resource_manager/resource_manager.hpp
+++ strapon/resource_manager/resource_manager.hpp
@@ -7,11 +7,12 @@
 #include <map>
 #include <iostream>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
 #ifdef __EMSCRIPTEN__
 #include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
 #else
 #include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
 #endif
 
 class ResourceManager {
