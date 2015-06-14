59d3013ad1ca0f19c2a3f0e5c8392dd5e95059f1
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-06-14 19:57:22 +0200

    ResourceManager: Add music handling

--- strapon/resource_manager/resource_manager.hpp
+++ strapon/resource_manager/resource_manager.hpp
@@ -4,6 +4,7 @@
 #include <SDL2/SDL.h>
 #include <string>
 #include <map>
+#include <iostream>
 #include <SDL2/SDL_image.h>
 #ifdef __EMSCRIPTEN__
 #include <SDL/SDL_mixer.h>
@@ -14,13 +15,11 @@
 class ResourceManager
 {
 public:
+	/***/
+	ResourceManager() {}
 
 	/***/
-	ResourceManager(){}
-	
-	/***/
-	~ResourceManager()
-	{
+	~ResourceManager() {
 		for(auto e : m_textures)
 		{
 			SDL_DestroyTexture(e.second);
@@ -34,59 +33,64 @@ public:
 	/***/
 	bool load_surface(const std::string &key,
 					  const std::string &path,
-					  SDL_Renderer *ren)
-	{
+					  SDL_Renderer *ren) {
 		SDL_Texture *texture;
 		texture = IMG_LoadTexture(ren, path.c_str());
-		if(!texture)
-		{
+
+		if (!texture) {
    			std::cerr << "IMG_Load:" << IMG_GetError() << std::endl;
    			return false;
 		}
+
 		m_textures[key] = texture;
 		return true;
 	}
-	
+
 	/***/
-	bool load_sound(std::string key, std::string &path)
-	{
+	bool load_sound(std::string key, std::string &path) {
 		Mix_Chunk *new_sound = Mix_LoadWAV(path.c_str());
-		if(new_sound == NULL)
-		{
+
+		if (new_sound == NULL) {
 			std::cerr << "Error: could not load sound from" << path << std::endl;
 			return false;
 		}
+
 		m_sounds[key] = new_sound;
 		return true;
 	}
 
 	/***/
-	SDL_Texture* get_const_texture(std::string texture_key) 
-	{
-		return m_textures.at(texture_key);
+	bool load_music(std::string key, std::string &path) {
+		Mix_Music *new_music = Mix_LoadMUS(path.c_str());
+
+		if (new_music == NULL) {
+			std::cerr << "Error: could not load sound from " << path << ": "<< Mix_GetError() << std::endl;
+			return false;
+		}
+
+		m_music[key] = new_music;
+		return true;
 	}
 
 	/***/
-	SDL_Texture* get_texture(std::string texture_key) 
-	{
+	SDL_Texture* get_texture(std::string texture_key) {
 		return m_textures.at(texture_key);
-	}
-
-	/***/
-	Mix_Chunk* get_const_sound(std::string sound_key) 
-	{
-		return m_sounds.at(sound_key);
 	}
 	
 	/***/
-	Mix_Chunk* get_sound(std::string sound_key) 
-	{
+	Mix_Chunk* get_sound(std::string sound_key) {
 		return m_sounds.at(sound_key);
 	}
 
+	/***/
+	Mix_Music* get_music(std::string music_key) {
+		return m_music.at(music_key);
+	}
+
 private:
-	std::map<std::string,SDL_Texture*> m_textures;
-	std::map<std::string,Mix_Chunk*> m_sounds;
+	std::map<std::string, SDL_Texture*> m_textures;
+	std::map<std::string, Mix_Chunk*> m_sounds;
+	std::map<std::string, Mix_Music*> m_music;
 };
 
 #endif
