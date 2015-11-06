6d3fa780dcd20307e7fe88a7620481a7d17e427c
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-11-06 21:33:13 +0100

    Fix sdl_helpers

--- strapon/sdl_helpers/sdl_helpers.hpp
+++ strapon/sdl_helpers/sdl_helpers.hpp
@@ -1,7 +1,7 @@
 #ifndef SDL_HELPER_HPP
 #define SDL_HELPER_HPP
 
-#include "resource_manager.hpp"
+#include "strapon/resource_manager/resource_manager.hpp"
 
 #include <SDL.h>
 #include <SDL_ttf.h>
@@ -10,24 +10,24 @@
  * Draws [text] to
  */
 
-static void draw_text(SDL_Renderer renderer, ResourceManager resource_manager, std::string text,
-                      std::string font_key, SDL_Rect &rect, SDL_Color color) {
+static void draw_text(SDL_Renderer *renderer, ResourceManager resource_manager, std::string text,
+                      std::string font_key, SDL_Rect *rect, SDL_Color color) {
     SDL_Surface *text_surface =
-        TFF_RenderText_Blended(resource_manager.get(font_key), text.c_str(), color);
+        TTF_RenderText_Blended(resource_manager.font(font_key), text.c_str(), color);
     SDL_Texture *text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
     SDL_RenderCopy(renderer, text_texture, nullptr, rect);
     SDL_FreeSurface(text_surface);
     SDL_DestroyTexture(text_texture);
 }
 
-static void draw_text(SDL_Renderer renderer, ResourceManager resource_manager, std::string text,
+static void draw_text(SDL_Renderer *renderer, ResourceManager resource_manager, std::string text,
                       std::string font_key, int x_pos, int y_pos, int char_width, int char_height,
                       SDL_Color color) {
-    SDL_Rect rect{x_pos, y_pos, char_width * text.length(), char_height};
+    SDL_Rect rect{x_pos, y_pos, char_width * int(text.length()), char_height};
     SDL_Surface *text_surface =
-        TFF_RenderText_Blended(resource_manager.get(font_key), text.c_str(), color);
+        TTF_RenderText_Blended(resource_manager.font(font_key), text.c_str(), color);
     SDL_Texture *text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
-    SDL_RenderCopy(renderer, text_texture, nullptr, rect);
+    SDL_RenderCopy(renderer, text_texture, nullptr, &rect);
     SDL_FreeSurface(text_surface);
     SDL_DestroyTexture(text_texture);
 }
